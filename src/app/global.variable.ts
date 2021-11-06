9a3c03bd39ceab433fe682be3719089ea8f0687a
jrughani <93567349+jrughani@users.noreply.github.com>
2021-11-06 18:16:51 -0400

    Update global.variable.ts

---
+++ src/app/global.variable.ts
@@ -0,0 +1,4 @@
+export const Title:string = "Wellness app";
+export const apiKey:string = "&apiKey=2748398f60524fc090d89b2009c9e4a1";
+export const timeFrameDay:string = "day";
+export const timeFrameWeek:string = "week";
